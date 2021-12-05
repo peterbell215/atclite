98b6cb2d1b5446b4895712944a5aeb0695025846
Peter Bell <peter.bell215@gmail.com>
2021-12-05 22:03:03 +0000

    Add ability to update aircraft position with tests

--- lib/ATC_lite/aircraft.rb
+++ lib/ATC_lite/aircraft.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 module ATCLite
+  # Detqils of an aircraft
   class Aircraft
     attr_accessor :callsign, :speed, :altitude, :heading, :x, :y
+
+    def initialize(callsign: 'BA001', speed: 250.0, altitude: 1000, heading: 0.0, x: 0, y: 0)
+      @callsign = callsign
+      @speed = speed
+      @altitude = altitude
+      @heading = heading
+      @x = x
+      @y = y
+    end
+
+    def update
+      heading_in_radians = self.heading * Math::PI / 180.0
+      distance_covered_in_1_s = self.speed / 3600.0
+
+      self.x += distance_covered_in_1_s * Math.sin(heading_in_radians)
+      self.y -= distance_covered_in_1_s * Math.cos(heading_in_radians)
+    end
   end
 end
---
+++ spec/ATC_lite/aircraft_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+
+RSpec.describe ATCLite::Aircraft do
+  describe '#update' do
+    shared_examples_for 'updated position based on heading' do |heading, x, y|
+      before do
+        aircraft.heading = heading
+        aircraft.update
+      end
+
+      it "calculates the updated position for #{heading}" do
+        expect(aircraft.x).to be_within(0.001).of(x)
+        expect(aircraft.y).to be_within(0.001).of(y)
+      end
+    end
+
+    subject(:aircraft) { ATCLite::Aircraft.new(speed: 3600) }
+    it_behaves_like 'updated position based on heading', 0.0, 0.0, -1.0
+    it_behaves_like 'updated position based on heading', 90.0, 1.0, 0.0
+    it_behaves_like 'updated position based on heading', 180.0, 0.0, 1.0
+    it_behaves_like 'updated position based on heading', 270.0, -1.0, 0.0
+  end
+end
