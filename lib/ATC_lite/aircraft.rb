bbc0b4fc9353ce1e2b68185785682aff921144f8
Peter Bell <peter.bell215@gmail.com>
2021-12-11 18:26:42 +0000

    Added Aircraft#update_heading

--- lib/ATC_lite/aircraft.rb
+++ lib/ATC_lite/aircraft.rb
@@ -3,7 +3,12 @@
 module ATCLite
   # Detqils of an aircraft
   class Aircraft
-    attr_accessor :callsign, :speed, :altitude, :heading, :x, :y
+    attr_reader :callsign, :speed, :heading, :altitude, :x, :y
+
+    attr_accessor :target_heading
+
+    KNOTS_TO_NM_PER_SECOND = 1.0/3600.00
+    TURN_RATE_PER_SECOND = 2
 
     def initialize(callsign: 'BA001', speed: 250.0, altitude: 1000, heading: 0.0, x: 0, y: 0)
       @callsign = callsign
@@ -17,12 +22,27 @@ module ATCLite
       @y = y
     end
 
+    # standard aircraft turn is 180 degrees per min, or 2 degrees per second
+    def update_heading
+      return if @target_heading == @heading
+
+      delta = (@target_heading - @heading)
+      delta += 360 if delta < -180
+      delta -= 360 if delta > 180
+
+      delta = [-TURN_RATE_PER_SECOND, delta].max
+      delta = [delta, TURN_RATE_PER_SECOND].min
+
+      @heading = (@heading + delta) % 360
+    end
+
+    # calculates, given a speed over ground, the updated position.
     def update_position
       heading_in_radians = self.heading * Math::PI / 180.0
-      distance_covered_in_1_s = self.speed / 3600.0
+      distance_covered_in_1_s = self.speed * KNOTS_TO_NM_PER_SECOND
 
-      self.x += distance_covered_in_1_s * Math.sin(heading_in_radians)
-      self.y -= distance_covered_in_1_s * Math.cos(heading_in_radians)
+      @x += distance_covered_in_1_s * Math.sin(heading_in_radians)
+      @y -= distance_covered_in_1_s * Math.cos(heading_in_radians)
     end
   end
 end
--- spec/ATC_lite/aircraft_spec.rb
+++ spec/ATC_lite/aircraft_spec.rb
@@ -1,23 +1,42 @@
 require 'rspec'
 
 RSpec.describe ATCLite::Aircraft do
-  describe '#update' do
+  describe '#update_position' do
     shared_examples_for 'updated position based on heading' do |heading, x, y|
-      before do
-        aircraft.heading = heading
-        aircraft.update_position
-      end
+      subject(:aircraft) { ATCLite::Aircraft.new(speed: 3600, heading: heading) }
 
       it "calculates the updated position for #{heading}" do
+        aircraft.update_position
+
         expect(aircraft.x).to be_within(0.001).of(x)
         expect(aircraft.y).to be_within(0.001).of(y)
       end
     end
 
-    subject(:aircraft) { ATCLite::Aircraft.new(speed: 3600) }
     it_behaves_like 'updated position based on heading', 0.0, 0.0, -1.0
     it_behaves_like 'updated position based on heading', 90.0, 1.0, 0.0
     it_behaves_like 'updated position based on heading', 180.0, 0.0, 1.0
     it_behaves_like 'updated position based on heading', 270.0, -1.0, 0.0
+  end
+
+  describe '#update_heading' do
+    shared_examples_for 'updated heading' do |current_heading, target_heading, new_heading|
+      subject(:aircraft) { ATCLite::Aircraft.new(heading: current_heading) }
+
+      it "calculates the updated heading when turning from #{current_heading} to #{target_heading}" do
+        aircraft.target_heading = target_heading
+        aircraft.update_heading
+        expect(aircraft.heading).to eq(new_heading)
+      end
+    end
+
+    subject(:aircraft) { ATCLite::Aircraft.new }
+
+    it_behaves_like 'updated heading', 270, 280, 272
+    it_behaves_like 'updated heading', 270, 271, 271
+    it_behaves_like 'updated heading', 270, 260, 268
+    it_behaves_like 'updated heading', 270, 269, 269
+    it_behaves_like 'updated heading', 359, 10, 1
+    it_behaves_like 'updated heading', 1, 350, 359
   end
 end
