95fdba2a1f0ed2fd669bd9a12c11fddce91f9e2c
Peter Bell <peter.bell215@gmail.com>
2021-12-18 04:50:10 +0000

    Moves test of whether aircraft performance data is complete to AircraftPerformanceEntry

--- lib/ATC_lite/aircraft_performance.rb
+++ lib/ATC_lite/aircraft_performance.rb
@@ -7,40 +7,20 @@ require_relative 'aircraft_performance_entry'
 module ATCLite
   # Class to model of a specific aircraft its performance data and provide this back to the simulator.
   class AircraftPerformance
-    VALID_DATA_FOR_PERFORMANCE_FILE = {
-      initial_climb: %i[ias roc upper_altitude],
-      climb: %i[ias roc lower_altitude],
-      cruise: %i[ias upper_altitude],
-      initial_descent: %i[roc lower_altitude],
-      descent: %i[roc ias upper_altitude lower_altitude],
-      approach: %i[roc ias upper_altitude lower_altitude]
-    }.freeze
-
     @performance_data = {}
+
+    def self.performance_data
+      @performance_data
+    end
 
     # Loads the performance data from the YAML file.
     def self.load_file(filename: 'data/aircraft_performance.yaml')
       loaded_file = YAML.load_file(filename)
 
       loaded_file.each_pair do |aircraft_type, overall_performance_data|
-        overall_performance_data.map! do |performance_data|
-          check_validity_of_performance_data(performance_data)
-          AircraftPerformanceEntry.new(**performance_data)
-        end
+        overall_performance_data.map! { |performance_data| AircraftPerformanceEntry.new(**performance_data) }
         @performance_data[aircraft_type] = overall_performance_data
       end
-    end
-
-    def self.performance_data
-      @performance_data
-    end
-
-    def self.check_validity_of_performance_data(performance_data)
-      raise InvalidPhaseError unless VALID_DATA_FOR_PERFORMANCE_FILE.key?(performance_data[:phase])
-
-      mandatory_attributes = VALID_DATA_FOR_PERFORMANCE_FILE[performance_data[:phase]]
-      missing_attributes = mandatory_attributes - performance_data.keys
-      missing_attributes.empty? || raise(MissingAttributesError.new(performance_data[:phase], missing_attributes))
     end
 
     def initialize(type)
@@ -53,16 +33,6 @@ module ATCLite
           altitude.between?(performance_entry.lower_altitude || 0.ft, performance_entry.upper_altitude || 100_000.ft)
       end
       data&.roc
-    end
-  end
-
-  # Error raised if the phase is invalid.
-  class InvalidPhaseError < StandardError; end
-
-  # Error raised if an attribute required for a specific phase is missing
-  class MissingAttributesError < StandardError
-    def initialize(phase, missing_attributes)
-      super("phase #{@phaser} is missing attributes #{missing_attributes}")
     end
   end
 end
--- spec/ATC_lite/aircraft_performance_entry_spec.rb
+++ spec/ATC_lite/aircraft_performance_entry_spec.rb
@@ -4,16 +4,25 @@ require 'rspec'
 
 RSpec.describe 'ATCLite::AircraftPerformanceEntry' do
   describe '#initialize' do
-    subject(:aircraft_performance_entry) do
-      ATCLite::AircraftPerformanceEntry.new(phase: :initial_climb, ias: 250, roc: 1000, lower_altitude: 4000,
-                                            upper_altitude: 6000)
-    end
+    context 'when the performance data is fully specified' do
+      subject(:aircraft_performance_entry) do
+        ATCLite::AircraftPerformanceEntry.new(phase: :initial_climb, ias: 250, roc: 1000, lower_altitude: 4000,
+                                              upper_altitude: 6000)
+      end
 
-    describe 'creates an entry from a hash' do
       specify { expect(aircraft_performance_entry.phase).to eq(:initial_climb) }
       specify { expect(aircraft_performance_entry.ias).to eq(250) }
       specify { expect(aircraft_performance_entry.roc).to eq(1000) }
       specify { expect(aircraft_performance_entry.lower_altitude).to eq(ATCLite::Altitude.new(4000)) }
+    end
+
+    context 'when the hash is incomplete' do
+      let(:parameters_with_missing_ias) { { phase: :initial_climb, roc: 1000, lower_altitude: 4000, upper_altitude: 6000 } }
+
+      it 'raises an appropriate error' do
+        expect { ATCLite::AircraftPerformanceEntry.new(**parameters_with_missing_ias) }
+          .to raise_error(ATCLite::MissingAttributesError)
+      end
     end
   end
 end
